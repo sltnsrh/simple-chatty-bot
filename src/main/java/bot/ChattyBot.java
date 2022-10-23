96fef3ee171422eda6808b7ed4ba7985ada944c8
sltnsrh <lifenjoy8888@gmail.com>
2022-10-23 21:13:41 +0300

    integer values input was fixed

--- src/main/java/bot/ChattyBot.java
+++ src/main/java/bot/ChattyBot.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class ChattyBot {
     final static Scanner scanner = new Scanner(System.in);
+    final static String assistantName = "Max";
+    final static String birthYear = "2020";
 
     public static void main(String[] args) {
-        greet("Max", "2020");
+        greet();
         remindName();
         guessAge();
         count();
         test();
     }
 
-    static void greet(String assistantName, String birthYear) {
+    static void greet() {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
@@ -21,22 +23,33 @@ public class ChattyBot {
 
     static void remindName() {
         String name = scanner.nextLine();
+        while (name.isBlank()) {
+            name = scanner.nextLine();
+        }
         System.out.println("What a great name you have, " + name + "!");
     }
 
     static void guessAge() {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        int remainder1 = scanner.nextInt();
-        int remainder2 = scanner.nextInt();
-        int remainder3 = scanner.nextInt();
+        int remainder1 = getIntegerFromInput();
+        int remainder2 = getIntegerFromInput();
+        int remainder3 = getIntegerFromInput();
         int age = (remainder1 * 70 + remainder2 * 21 + remainder3 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 
+    static int getIntegerFromInput() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Please, try again with a numeric symbol: ");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
     static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
-        int num = scanner.nextInt();
+        int num = getIntegerFromInput();
         for (int i = 0; i <= num; i++) {
             System.out.printf("%d!\n", i);
         }
@@ -44,7 +57,7 @@ public class ChattyBot {
 
     static void test() {
         System.out.println("Let's test your programming knowledge.");
-        int answer = scanner.nextInt();
+        int answer = getIntegerFromInput();
         if (answer == 2) {
             end();
         } else {
